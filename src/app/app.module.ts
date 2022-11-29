939eaf8276701f49398cdb979a3488d01e45c066
diegocolonia <diegocolonia98@yahoo.com>
2022-11-29 12:50:36 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+const routers: Routes=[
+  {path:'product',component: ProductComponent},
+  {path:'product-list',component: ProductListComponent},
+  {path:'',redirectTo:'/home',pathMatch:'full'},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routers),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { Product } from '../models/product';
+import { response } from 'express';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseURl= 'http://localhost:8080/api/products'
+
+  constructor(private _httpClient: HttpClient) { }
+
+
+  getProductList(): Observable<Product[]>{
+    return this._httpClient.get<getResponse>(this.baseURl).pipe(
+      map(response=>response._embedded.products)
+    )
+  }
+
+
+}
+
+interface getResponse{
+  _embedded:{
+    products: Product[]
+  }
+}
