5188719e44329b194accd02a891639bb58a48904
diegocolonia <diegocolonia98@yahoo.com>
2022-12-02 22:23:21 -0500

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import {HttpClientModule} from "@angular/common/http";
 import { RouterModule, Routes } from '@angular/router';
 
 import { ProductCategoryMedu2Component } from './components/product-category-medu2/product-category-medu2.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 
 
 const routers: Routes=[
+  {path:'search/:keyword',component:ProductListComponent},
   {path:'category/:id',component: ProductListComponent},
   {path:'category',component: ProductListComponent},
   {path:'',component: ProductListComponent},
@@ -27,6 +29,7 @@ const routers: Routes=[
     ProductListComponent,
   
     ProductCategoryMedu2Component,
+       SearchComponent,
 
   ],
   imports: [
